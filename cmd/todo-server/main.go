8af90055f05a8492b878a56178dc93de2383b791
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2024-05-25 22:16:02 +0530

    added db setup

--- cmd/todo-server/main.go
+++ cmd/todo-server/main.go
@@ -1,20 +1,55 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
 	"todo-server/api"
+
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	connStr := os.Getenv("POSTGRES_CONNECTION_STRING")
+
+	if connStr == "" {
+		log.Fatal("POSTGRES_CONNECTION_STRING environment variable not set")
+	}
+
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		fmt.Println("Failed to connect to database", err)
+		return
+	}
+	defer db.Close()
+
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
 
 	api.SetupRoutes(r)
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Ping failed", err)
+		return
+	}
+
+	fmt.Println("Connected to the database")
 
 	http.ListenAndServe(":3000", r)
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.22.3
 
 require github.com/go-chi/chi/v5 v5.0.12
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
