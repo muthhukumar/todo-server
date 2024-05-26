73df893dca083e0d37062c86d3cf242e99d94fdc
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2024-05-26 10:36:50 +0530

    updated tasks table query

--- queries.sql
+++ queries.sql
@@ -2,7 +2,6 @@ CREATE TABLE tasks (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     completed BOOLEAN NOT NULL DEFAULT false,
-    completed_on DATE
+    completed_on VARCHAR(255) NOT NULL DEFAULT '',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-ALTER TABLE tasks ALTER COLUMN completed_on TYPE TEXT;
