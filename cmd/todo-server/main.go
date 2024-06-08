02347a76413ccbb149f375215b71a141b6fbbd8a
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2024-06-08 15:49:01 +0530

    added api rate limit

--- cmd/todo-server/main.go
+++ cmd/todo-server/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/httprate"
 	"github.com/rs/cors"
 
 	"todo-server/api"
@@ -20,6 +22,7 @@ func main() {
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
+	r.Use(httprate.LimitByIP(100, 1*time.Minute))
 
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:1420", "*"},
@@ -32,10 +35,11 @@ func main() {
 
 	api.SetupRoutes(r, db)
 
-	// internal.SetupCronJobs(db)
+	// emailAuth := internal.LoadEmailCredentials()
+
+	// internal.SetupCronJobs(db, emailAuth)
 
 	// Enable it when email credentials is needed
-	// internal.LoadEmailCredentials()
 
 	http.ListenAndServe(":3000", r)
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.22.3
 require github.com/go-chi/chi/v5 v5.0.12
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/go-chi/httprate v0.9.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
