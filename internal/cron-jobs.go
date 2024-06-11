96a9281c07b9ce35fe96ce4abe19e35e7843974d
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2024-06-11 21:56:43 +0530

    added a cron job for completed tasks

--- internal/cron-jobs.go
+++ internal/cron-jobs.go
@@ -12,8 +12,7 @@ import (
 func SetupCronJobs(db *sql.DB, emailAuth models.EmailAuth) {
 	c := cron.New(cron.WithSeconds())
 
-	c.AddFunc("0 0 7 * * *", func() {
-		// c.AddFunc("*/60 * * * * *", func() {
+	c.AddFunc("0 30 1 * * *", func() {
 		today := time.Now().Format("2006-01-02")
 
 		query := "select name, due_date from tasks where due_date = $1 ORDER BY created_at DESC"
@@ -56,6 +55,49 @@ func SetupCronJobs(db *sql.DB, emailAuth models.EmailAuth) {
 		email_sent := SendEmail(emailAuth, template)
 
 		fmt.Println("Email send", email_sent, time.Now())
+	})
+
+	c.AddFunc("0 30 16 * * *", func() {
+		query := "SELECT name FROM tasks WHERE completed = true AND DATE(completed_on) = CURRENT_DATE;"
+
+		rows, err := db.Query(query)
+
+		if err != nil {
+			fmt.Println("Failed to run the query", err.Error())
+			return
+		}
+
+		var tasks []models.Task
+
+		for rows.Next() {
+			var task models.Task
+			if err := rows.Scan(&task.Name); err != nil {
+				fmt.Println("Failed to set task data")
+				return
+			}
+
+			tasks = append(tasks, task)
+		}
+		defer rows.Close()
+
+		var body = fmt.Sprintf("Tasks completed Today: %v", time.Now().Format("Monday, January 2 2006"))
+
+		body += "\n"
+		body += "\n"
+		for idx, task := range tasks {
+			body += fmt.Sprintf("%d. %s", idx+1, task.Name)
+			body += "\n"
+		}
+
+		template := models.EmailTemplate{
+			To:      []string{emailAuth.ToEmail},
+			Subject: "Tasks completed Today",
+			Body:    body,
+		}
+
+		email_sent := SendEmail(emailAuth, template)
+
+		fmt.Println("Email send for completed tasks", email_sent, time.Now())
 	})
 
 	c.Start()
