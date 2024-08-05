96c2d4ccd9e4e0173f1c979c9c0c339f59258f2c
muthhukumar <muthukumarperiyasamy.mk@gmail.com>
2024-08-05 09:02:01 +0530

    right now we're getting all the quotes from the notion database

--- data/thoughts/notion.go
+++ data/thoughts/notion.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -8,6 +9,11 @@ import (
 	"net/http"
 	"os"
 )
+
+// TODO: maybe rename this thing here later. Not sure what to rename but in the payload we have to send it as start cursor but we get the value as next_cursor
+type NextCursorPayload struct {
+	NextCursor string `json:"start_cursor"`
+}
 
 type NotionBaseResponseResultPropertiesNameTitle struct {
 	PlainText string `json:"plain_text"`
@@ -39,48 +45,74 @@ func GetQuotesFromNotion() ([]string, error) {
 
 	client := &http.Client{}
 
-	req, err := http.NewRequest("POST", url, nil)
-
-	if err != nil {
-		return nil, err
-	}
-
-	bearerToken := fmt.Sprintf("Bearer %s", notionSecretToken)
-
-	req.Header.Add("Content-Type", "application/json; charset=UTF-8")
-	req.Header.Add("Authorization", bearerToken)
-	req.Header.Add("Notion-Version", "2022-06-28")
-
-	resp, err := client.Do(req)
-
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		bodyBytes, err := io.ReadAll(resp.Body)
-
-		if err != nil {
-			return nil, errors.New(string(bodyBytes))
-		}
-	}
-
-	notionResponse := &NotionBaseResponse{}
-
-	decodeErr := json.NewDecoder(resp.Body).Decode(notionResponse)
-
-	if decodeErr != nil {
-		return nil, decodeErr
-	}
+	hasMore := true
+	iterations, ITERATION_LIMIT := 0, 5
+	var nextCursor string
 
 	var quotes []string
 
-	for _, result := range notionResponse.Results {
-		for _, title := range result.Properties.Name.Title {
-			quotes = append(quotes, title.PlainText)
+	for hasMore && iterations <= ITERATION_LIMIT {
+		jsonData, err := json.Marshal(createPayload(nextCursor))
+
+		if err != nil {
+			return nil, err
 		}
+
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+
+		if err != nil {
+			return nil, err
+		}
+
+		bearerToken := fmt.Sprintf("Bearer %s", notionSecretToken)
+
+		req.Header.Add("Content-Type", "application/json; charset=UTF-8")
+		req.Header.Add("Authorization", bearerToken)
+		req.Header.Add("Notion-Version", "2022-06-28")
+
+		resp, err := client.Do(req)
+
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			bodyBytes, err := io.ReadAll(resp.Body)
+
+			if err != nil {
+				return nil, errors.New(string(bodyBytes))
+			}
+		}
+
+		notionResponse := &NotionBaseResponse{}
+
+		decodeErr := json.NewDecoder(resp.Body).Decode(notionResponse)
+
+		if decodeErr != nil {
+			return nil, decodeErr
+		}
+
+		for _, result := range notionResponse.Results {
+			for _, title := range result.Properties.Name.Title {
+				quotes = append(quotes, title.PlainText)
+			}
+		}
+
+		hasMore = notionResponse.HasMore
+		nextCursor = notionResponse.NextCursor
+
+		iterations += 1
 	}
 
 	return quotes, nil
+}
+
+func createPayload(nextCursor string) interface{} {
+	if nextCursor == "" {
+		return map[string]string{}
+	}
+
+	return NextCursorPayload{NextCursor: nextCursor}
+
 }
