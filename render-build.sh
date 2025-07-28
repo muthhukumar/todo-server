099edd7e01b75fc7e26d45a16f6a97981d1ecc0d
muthhukumar <muthukumarperiyasamy.mk@gmail.com>
2025-07-28 21:23:43 +0530

    do not setup  chrome

--- render-build.sh
+++ render-build.sh
@@ -2,18 +2,18 @@
 # exit on error
 set -o errexit
 
-STORAGE_DIR=/opt/render/project/go/src/github.com/muthhukumar/todo-server/.render
+# STORAGE_DIR=/opt/render/project/go/src/github.com/muthhukumar/todo-server/.render
 
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "...Downloading Chrome"
-  mkdir -p $STORAGE_DIR/chrome
-  cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/go/src/github.com/muthhukumar/todo-server
-else
-  echo "...Using Chrome from cache"
-fi
+# if [[ ! -d $STORAGE_DIR/chrome ]]; then
+#   echo "...Downloading Chrome"
+#   mkdir -p $STORAGE_DIR/chrome
+#   cd $STORAGE_DIR/chrome
+#   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+#   rm ./google-chrome-stable_current_amd64.deb
+#   cd $HOME/project/go/src/github.com/muthhukumar/todo-server
+# else
+#   echo "...Using Chrome from cache"
+# fi
 
 go build ./cmd/todo-server/main.go
